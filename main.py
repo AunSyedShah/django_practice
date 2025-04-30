b5ba2d78a161590467ea161b34e21fc84c52456a
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-04-30 15:40:03 +0000

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+class Person():
+    def __init__(self):
+        self.name = "Noman"
+    
+    def whatsAppAudioCall(self):
+        print(self.name)
+
+p1 = Person()
+p1.name = "Zohaib"
+p1.whatsAppAudioCall()
---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Person
+
+# Create your views here.
+def home(request):
+    persons = Person.objects.all()
+    context = {
+        "persons": persons
+    }
+    return render(request, "home.html", context)
+
+def contact(request):
+    return render(request, "contact.html")
