ce3de4affafa12dc73713b7cb0c9925a43b29d85
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-02 13:46:31 +0000

    new column in models

--- main_app/models.py
+++ main_app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    name = models.CharField(max_length=100)
-    contactNumber = models.CharField(max_length=20, verbose_name="Contact Number")
+    name = models.CharField(max_length=100, default='None')
+    contactNumber = models.CharField(max_length=20, verbose_name="Contact Number", default='None')
+    address = models.TextField(default='None')
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -3,14 +3,13 @@ from .models import Person
 
 # Create your views here.
 def home(request):
+    context = {}
     if request.method == "POST":
         contactName = request.POST['contactName']
         contactNumber = request.POST['contactNumber']
         Person.objects.create(name=contactName, contactNumber=contactNumber)
     persons = Person.objects.all()
-    context = {
-        "persons": persons
-    }
+    context['persons'] = persons
     return render(request, "home.html", context)
 
 def contact(request):
