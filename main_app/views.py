666c47c534670d6b330dbda63a7fc5827e318e2c
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-07 13:33:17 +0000

    djano model form

--- main_app/views.py
+++ main_app/views.py
@@ -4,16 +4,18 @@ from django.core import serializers
 import json
 
 from .models import Person
+from .forms import PersonModelForm
 
 # Create your views here.
 def home(request):
     context = {}
     if request.method == "POST":
-        contactName = request.POST['contactName']
-        contactNumber = request.POST['contactNumber']
-        Person.objects.create(name=contactName, contactNumber=contactNumber)
+        form = PersonModelForm(request.POST)
+        if form.is_valid():
+            form.save()
     persons = Person.objects.all()
     context['persons'] = persons
+    context['form'] = PersonModelForm()
     return render(request, "home.html", context)
 
 def contact(request):
